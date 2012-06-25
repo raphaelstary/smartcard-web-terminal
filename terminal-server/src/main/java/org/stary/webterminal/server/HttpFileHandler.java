9bc6532d7978c7e6997bfd84b3d40cdbbff638e7
raphael <raphael.stary@gmx.at>
2012-06-26 00:26:53 +0200

    basic http file server

---
+++ terminal-server/src/main/java/org/stary/webterminal/server/HttpFileHandler.java
@@ -0,0 +1,141 @@
+package org.stary.webterminal.server;
+
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.*;
+import org.jboss.netty.handler.codec.frame.TooLongFrameException;
+import org.jboss.netty.handler.codec.http.*;
+import org.jboss.netty.util.CharsetUtil;
+
+import javax.activation.MimetypesFileTypeMap;
+import java.io.*;
+import java.net.URLDecoder;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
+/**
+ * @author raphael
+ */
+public class HttpFileHandler extends SimpleChannelUpstreamHandler {
+
+    @Override
+    public void messageReceived(ChannelHandlerContext ctx, MessageEvent event) throws IOException {
+        HttpRequest request = (HttpRequest) event.getMessage();
+
+        if (request.getMethod() != HttpMethod.GET) {
+            sendError(ctx, HttpResponseStatus.METHOD_NOT_ALLOWED);
+            return;
+        }
+
+        final String path = sanitizeUri(request.getUri());
+
+        if (path == null) {
+            sendError(ctx, HttpResponseStatus.FORBIDDEN);
+            return;
+        }
+
+        File file = new File(path);
+        if (file.isHidden() || !file.exists()) {
+            sendError(ctx, HttpResponseStatus.NOT_FOUND);
+            return;
+        }
+
+        if (!file.isFile()) {
+            sendError(ctx, HttpResponseStatus.FORBIDDEN);
+            return;
+        }
+
+        RandomAccessFile raf;
+        try {
+            raf = new RandomAccessFile(file, "r");
+        } catch (FileNotFoundException e) {
+            sendError(ctx, HttpResponseStatus.NOT_FOUND);
+            return;
+        }
+
+        long fileLength = raf.length();
+
+        HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+        HttpHeaders.setContentLength(response, fileLength);
+        setContentTypeHeader(response, file);
+        setDateHeaders(response);
+
+        Channel channel = ctx.getChannel(); //event.getChannel();
+
+        channel.write(response);
+//        ctx.getChannel();
+
+
+        final FileRegion region = new DefaultFileRegion(raf.getChannel(), 0, fileLength);
+        ChannelFuture writeFuture = channel.write(region);
+
+        writeFuture.addListener(new ChannelFutureProgressListener() {
+            @Override
+            public void operationProgressed(ChannelFuture future, long amount, long current, long total) {
+                System.out.printf("%s: %d / %d (+%d)%n", path, current, total, amount);
+            }
+
+            @Override
+            public void operationComplete(ChannelFuture future) {
+                region.releaseExternalResources();
+            }
+        });
+
+        if (!HttpHeaders.isKeepAlive(request))
+            writeFuture.addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent event) {
+        Channel channel = event.getChannel();
+        Throwable cause = event.getCause();
+        if (cause instanceof TooLongFrameException) {
+            sendError(ctx, HttpResponseStatus.BAD_REQUEST);
+            return;
+        }
+
+        cause.printStackTrace();
+        if (channel.isConnected()) {
+            sendError(ctx, HttpResponseStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private static void sendError(ChannelHandlerContext ctx, HttpResponseStatus status) {
+        HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, status);
+        response.setHeader(HttpHeaders.Names.CONTENT_TYPE, "text/plain; charset=UTF-8");
+        response.setContent((ChannelBuffers.copiedBuffer("Failure: " + status.toString() + "\r\n", CharsetUtil.UTF_8)));
+
+        ctx.getChannel().write(response).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    private static String sanitizeUri(String uri) {
+        try {
+            uri = URLDecoder.decode(uri, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            try {
+                uri = URLDecoder.decode(uri, "ISO-8859-1");
+            } catch (UnsupportedEncodingException e1) {
+                throw new Error();
+            }
+        }
+
+        uri = uri.replace('/', File.separatorChar);
+
+        return System.getProperty("user.dir") + File.separator + uri;
+    }
+
+    private static void setContentTypeHeader(HttpResponse response, File file) {
+        MimetypesFileTypeMap map = new MimetypesFileTypeMap();
+        response.setHeader(HttpHeaders.Names.CONTENT_TYPE, map.getContentType(file.getPath()));
+    }
+
+    private static void setDateHeaders(HttpResponse response) {
+        SimpleDateFormat formatter = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US);
+        formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        Calendar time = new GregorianCalendar();
+        response.setHeader(HttpHeaders.Names.DATE, formatter.format(time.getTime()));
+    }
+}
---
+++ terminal-server/src/main/java/org/stary/webterminal/server/SmartCardTerminalServer.java
@@ -0,0 +1,56 @@
+package org.stary.webterminal.server;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.jboss.netty.handler.codec.http.HttpChunkAggregator;
+import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
+import org.jboss.netty.handler.codec.http.HttpRequestEncoder;
+import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
+import org.jboss.netty.handler.stream.ChunkedWriteHandler;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+/**
+ * @author raphael
+ */
+public class SmartCardTerminalServer {
+    private final int httpPort;
+
+    public SmartCardTerminalServer(int httpPort) {
+        this.httpPort = httpPort;
+    }
+
+    public void run() {
+        ServerBootstrap bootstrap = new ServerBootstrap(
+                new NioServerSocketChannelFactory(Executors.newCachedThreadPool(), Executors.newCachedThreadPool()));
+
+        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+            @Override
+            public ChannelPipeline getPipeline() throws Exception {
+                ChannelPipeline pipeline = Channels.pipeline();
+
+                pipeline.addLast("decoder", new HttpRequestDecoder());
+                pipeline.addLast("aggregator", new HttpChunkAggregator(65536));
+                pipeline.addLast("encoder", new HttpResponseEncoder());
+                pipeline.addLast("chunkedWriter", new ChunkedWriteHandler());
+
+                pipeline.addLast("handler", new HttpFileHandler());
+
+                return pipeline;
+            }
+        });
+
+        bootstrap.bind(new InetSocketAddress(httpPort));
+    }
+
+    public static void main(String[] args) {
+        if (args.length > 0)
+            new SmartCardTerminalServer(Integer.parseInt(args[0])).run();
+        else
+            new SmartCardTerminalServer(8080).run();
+    }
+}
