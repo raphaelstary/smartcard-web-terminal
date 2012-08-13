768b0c468c8c2e2b4ee72ddfce36707056113437
raphaelstary <raphael.stary@gmx.at>
2012-08-13 15:04:48 +0200

    getting near from cost to cost

--- terminal-server/src/main/java/org/stary/webterminal/server/RestApiHandler.java
+++ terminal-server/src/main/java/org/stary/webterminal/server/RestApiHandler.java
@@ -1,7 +1,15 @@
 package org.stary.webterminal.server;
 
-import org.jboss.netty.channel.ChannelHandlerContext;
-import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.*;
+import org.jboss.netty.handler.codec.http.*;
+import org.jboss.netty.util.CharsetUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.stary.webterminal.server.HttpUtils.setContentTypeHeader;
+import static org.stary.webterminal.server.HttpUtils.setDateHeaders;
 
 /**
  * RestApiHandler
@@ -9,6 +17,22 @@ import org.jboss.netty.channel.MessageEvent;
 public class RestApiHandler implements BusinessLogicHandler {
     @Override
     public void handleRequest(ChannelHandlerContext ctx, MessageEvent event) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+        response.setHeader(HttpHeaders.Names.CONTENT_TYPE, "application/json; charset=UTF-8");
+        setDateHeaders(response);
+
+        StringBuilder json = new StringBuilder();
+        json.append("{ 'channels' : [");
+        for (Channel channel : SmartCardTerminalServer.allChannels) {
+            json.append("{'channel' : '").append(channel.toString()).append("'},");
+        }
+        json.append("]}");
+
+        response.setContent(ChannelBuffers.copiedBuffer(json.toString(), CharsetUtil.UTF_8));
+
+
+        Channel channel = event.getChannel();
+
+        channel.write(response);
     }
 }
