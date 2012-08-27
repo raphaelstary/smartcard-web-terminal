7571e0181f910d45398c996de6d64fe52208d5dc
raphaelstary <raphael.stary@gmx.at>
2012-08-27 09:01:17 +0200

    eliminated sammy-js dependency

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,33 +2,35 @@ function ConnectViewModel() {
     var self = this;
 
     // data
-    self.readerList = ko.observable({channels:[{id:"666"},{id:"667"},{id:"668"}]});
+    self.readerList = ko.observable({channels:[
+        {id:"666"},
+        {id:"667"},
+        {id:"668"}
+    ]});
     self.chosenReaderId = ko.observable();
     self.chosenReaderData = ko.observable();
     self.command = ko.observable();
     self.showReaderArea = ko.observable(false);
 
     // behaviours
+    self.init = function () {
+        $.ajax({
+            type:'POST',
+            url:'/api',
+            contentType:"application/json",
+            dataType:"json",
+            data:JSON.stringify({action:"getPcscClients"}),
+            success:function (data) {
+                self.readerList(data);
+            }
+        });
+    };
+
     self.connectToReader = function () {
 
     };
+
     self.sendCommand = function () {
 
     };
-
-    Sammy(function () {
-        this.get('', function () {
-            $.ajax({
-                type:'POST',
-                url:'/api',
-                contentType:"application/json",
-                dataType:"json",
-                data:JSON.stringify({action:"getPcscClients"}),
-                success:function (data) {
-                    self.readerList(data);
-                }
-            });
-        });
-
-    }).run();
 }
